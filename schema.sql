b6e5e7cedf920a18b4610fb231b5a1da5dc505eb
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-07-12 20:21:39 -0700

    uploading retirement_info.csv file

--- schema.sql
+++ schema.sql
@@ -69,5 +69,11 @@ SELECT * FROM dept_manager;
 SELECT * FROM salaries;
 SELECT * FROM titles;
 
-
-
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
