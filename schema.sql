56f32a716ce694696b60fb982c5c3008ffe6bdd1
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-07-12 19:29:47 -0700

    updating schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+DROP TABLE employees CASCADE;
+DROP TABLE departments CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE dept_emp CASCADE; 
+DROP TABLE salaries CASCADE;
+DROP TABLE titles CASCADE;
+
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -7,7 +15,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -35,4 +43,31 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
+);
+
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+
+
