68d58b321fac57a3c35436e00114db68af8da062
carlosalexismorales <102444078+carlosalexismorales@users.noreply.github.com>
2022-07-13 18:19:00 -0700

    Updating challenge code

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,10 +1,64 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--CHALLENGE
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+--TABLE 1 - Number of [Titles] Retiring
+--create a Retirement Titles table that holds all the titles of current employees 
+--who were born between January 1, 1952 and December 31, 1955.
+
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees AS e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+--Table 3
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles;
+
+--Deliverable 2
+--Mentorship Eligibility table
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+--DISTINCT ON(emp_no)
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS t
+				ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility;
